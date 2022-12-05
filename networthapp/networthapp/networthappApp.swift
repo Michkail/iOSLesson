0ee7bdac5830ecd1e5fbfc8f9d5246238fa67f17
Michkail <55208634+Michkail@users.noreply.github.com>
2022-12-05 15:16:02 +0700

    starting xcode

---
+++ networthapp/networthapp/networthappApp.swift
@@ -0,0 +1,17 @@
+//
+//  networthappApp.swift
+//  networthapp
+//
+//  Created by Stuckware on 05/12/22.
+//
+
+import SwiftUI
+
+@main
+struct networthappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
